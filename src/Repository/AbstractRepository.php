9ac4d38e14107e9c9bac29dfac867791cd618800
Michał Surmaczewski <github@msurma.net>
2024-01-24 03:58:10 +0100

    Add AbstractRepository

---
+++ src/Repository/AbstractRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Image;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+abstract class AbstractRepository extends ServiceEntityRepository
+{
+}
